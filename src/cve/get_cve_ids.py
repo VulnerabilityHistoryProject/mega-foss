b678b26a309c962cbcf9df7d447929ce25afbdd7
Tyler <txj3401@rit.edu>
2024-09-26 17:04:46 -0400

    added better processing of cvelistV5 to get_cve_ids.py.

--- src/cve/get_cve_ids.py
+++ src/cve/get_cve_ids.py
@@ -11,29 +11,29 @@ print(f"Reading CVE repo from {cvelist}")
 cve_ids = pathlib.Path('/cve_ids.csv')
 # cve_id_output = open(cve_ids, 'x')
 
-cvelist_path = pathlib.Path(cvelist)
-for p in cvelist_path.rglob("CVE*.json"):
-	with open(p, 'r') as cve_file:
-		try:
-			data = orjson.loads(cve_file.read())
-			id = data['CVE_data_meta']['ID']
-			state = data['CVE_data_meta']['STATE']
-			if state != 'REJECT' and state != 'RESERVED':
-				vendor_data = data['affects']['vendor']['vendor_data']
-				for vendor in vendor_data:
-					vendor_name = vendor['vendor_name']
-					if vendor_name != 'n/a':
-						product_data = vendor['product']['product_data']
-						for product in product_data:
-							product_name = product['product_name']
-							print(','.join([id, vendor_name, product_name]))
-							# TODO: sort by vendor and product
-						# print('\n')
-		except UnicodeDecodeError as e:
-			print(e.reason)
-			print(f"ERROR loading {p}")
-		except KeyError as e:
-			print("Key not found in " + cve_file.name)
+# cvelist_path = pathlib.Path(cvelist)
+# for p in cvelist_path.rglob("CVE*.json"):
+# 	with open(p, 'r') as cve_file:
+# 		try:
+# 			data = orjson.loads(cve_file.read())
+# 			id = data['CVE_data_meta']['ID']
+# 			state = data['CVE_data_meta']['STATE']
+# 			if state != 'REJECT' and state != 'RESERVED':
+# 				vendor_data = data['affects']['vendor']['vendor_data']
+# 				for vendor in vendor_data:
+# 					vendor_name = vendor['vendor_name']
+# 					if vendor_name != 'n/a':
+# 						product_data = vendor['product']['product_data']
+# 						for product in product_data:
+# 							product_name = product['product_name']
+# 							print(','.join([id, vendor_name, product_name]))
+# 							# TODO: sort by vendor and product
+# 						# print('\n')
+# 		except UnicodeDecodeError as e:
+# 			print(e.reason)
+# 			print(f"ERROR loading {p}")
+# 		except KeyError as e:
+# 			print("Key not found in " + cve_file.name)
 
 cvelistV5 = os.path.join(os.path.dirname(__file__), '../../../cvelistV5/cves')
 print(f"Reading CVE repo from {cvelistV5}")
@@ -42,22 +42,17 @@ cvelistV5_path = pathlib.Path(cvelistV5)
 for p in cvelistV5_path.rglob("CVE*.json"):
 	with open(p, 'r') as cve_file:
 		try:
-			data = orjson.loads(cve_file.read())["x_legacyV4Record"]
-			id = data['CVE_data_meta']['ID']
-			state = data['CVE_data_meta']['STATE']
+			data = orjson.loads(cve_file.read())
+			id = data['cveMetadata']['cveId']
+			state = data['cveMetadata']['state']
 			if state != 'REJECT' and state != 'RESERVED':
-				vendor_data = data['affects']['vendor']['vendor_data']
-				for vendor in vendor_data:
-					vendor_name = vendor['vendor_name']
-					if vendor_name != 'n/a':
-						product_data = vendor['product']['product_data']
-						for product in product_data:
-							product_name = product['product_name']
-							print(','.join([id, vendor_name, product_name]))
-							# TODO: sort by vendor and product
-						# print('\n')
+				vendor_data = data['cveMetadata']['assignerOrgId']
+				vendor_name = data['cveMetadata']['assignerShortName']
+				if vendor_name != 'n/a':
+					print(','.join([id, vendor_name]))
+					# print('\n')
 		except UnicodeDecodeError as e:
 			print(e.reason)
 			print(f"ERROR loading {p}")
-		except KeyError as e:
-			print("Key not found in " + cve_file.name)
+		except KeyError as k:
+			print(str(k) +  " not found in " + cve_file.name)
