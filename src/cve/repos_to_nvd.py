09cb747f1b32bb67772b94a5126112fd820f48ac
Aiden Green <70248258+blaqat@users.noreply.github.com>
2024-10-02 17:53:36 -0400

    Switched Repos_To_Nvd to use Postgres 17
    - Tested postgres17 json_table loader
    - Reimplmeneted repos_to_nvd using local database
    - Result is much more consistent and cleaner.

--- src/cve/repos_to_nvd.py
+++ src/cve/repos_to_nvd.py
@@ -8,6 +8,7 @@ import pathlib
 import os
 import re
 import orjson
+import psycopg2
 from tqdm import tqdm
 
 # Input files/folders
@@ -20,66 +21,61 @@ output_file = os.path.join(os.path.dirname(__file__), 'output/repos_to_nvd.csv')
 missing_file = os.path.join(os.path.dirname(__file__), 'output/missing_repos.txt')
 fix_file = os.path.join(os.path.dirname(__file__), 'output/manual_fix_repos.txt')
 
-URL_REGEX = re.compile(r"https?://(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)")
+# Connection details
+conn = psycopg2.connect(
+    dbname="cve_db",
+    user="postgres",
+    password=PASSWORD,# <--- Change this to your password
+    host="localhost"
+)
+
+# QUERIES
+FILTER_CVE_DATA = """
+SELECT * FROM cve_data
+WHERE (vendor ILIKE %s OR urls::TEXT ILIKE %s)
+   AND (product ILIKE %s OR product ILIKE %s);
+"""
 
 class Repo:
 	ids: tuple
 	def __init__(self, name=None, vendor=None, url=None):
-		self.ids = (str(name).lower(), str(vendor).lower())
+		self.ids = (str(vendor).lower(), str(name).lower())
 		self.url = url
 		self.matches = list()
-		self.cve_vendor = list()
-		self.cve_product = list()
+		self.state = 0
+		self.cve_vendor = None
+		self.cve_product = None
 
-	def match_with(self, vendors, products, urls):
-		for i in range(len(vendors)):
-			v = str(vendors[i])
-			p = str(products[i])
-			if v.lower() in self.ids and p.lower() in self.ids:
-				return self.add_match([v], [p], [])
-
-		url_matches = []
-		for url in urls:
-			if f"https://github.com/{self.url}".lower() in url.lower():
-				url_matches.append(url)
-
-
-		if url_matches and len(url_matches) > 0:
-			self.add_match(vendors, products, url_matches)
-
-	def add_match(self, vendor, product, urls):
-		for u in urls:
-			self.matches.append(u)
-		for p in product:
-			self.cve_product.append(p)
-		for v in vendor:
-			self.cve_vendor.append(v)
-
+	def add_match(self, row):
+		cve_id, vendor, product, urls = row
+		self.matches.append((vendor, product))
 
 	def resolve(self):
-		for i in range(len(self.cve_vendor)):
-			if i >= len(self.cve_vendor):
-				break
-			ven = str(self.cve_vendor[i])
-			prod = str(self.cve_product[i])
-			if ven.lower() == self.ids[1] and prod.lower() == self.ids[0]:
-				self.cve_vendor = [ven]
-				self.cve_product = [prod]
+		matches_set = set([(a.lower(), b.lower()) for a,b in filter(lambda match: match[0] != 'n/a', self.matches)])
+		len_matches = len(matches_set)
 
-		for i in range(len(self.cve_product)):
-			if i >= len(self.cve_product):
-				break
-			prod = self.cve_product[i]
-			ven = self.cve_vendor[i]
-			if str(self.url).lower() == str(prod).lower():
-				self.cve_vendor = [ven]
-				self.cve_product = [prod]
+		if len_matches == 0:
+			self.state = 0
+		elif len_matches > 1:
+			if self.ids in matches_set:
+				self.state = 2
+				self.cve_vendor, self.cve_product = self.ids
+			else:
+				self.state = 1
+		else:
+			self.state = 2
+			self.cve_vendor, self.cve_product = self.matches[0]
 
 	def __str__(self):
 		out = f"[ {self.url} ]\n"
-		out += f"Vendor: {set(self.cve_vendor)}\nProduct: {set(self.cve_product)}\n"
-		return out + "-"*80
+		if self.cve_vendor is not None and self.cve_product is not None:
+			out += f"Vendor: {self.cve_vendor}\nProduct: {self.cve_product}\n"
+		else:
+			vendors = [vendor for vendor, _ in self.matches]
+			products = [product for _, product in self.matches]
+			out += f"Vendor: {list(vendors)}\nProduct: {list(products)}\n"
 
+		return out + "-"*80
 
 	def __repr__(self):
 		return self.__str__()
@@ -98,29 +94,15 @@ def read_data():
 
 	return repos, id_map
 
-def parse_jsons(repos):
-	cvelist_path = pathlib.Path(cvelist)
-	for p in tqdm(list(cvelist_path.rglob("CVE*.json")), desc="Parsing JSONs"):
-		with open(p, 'r') as f:
-			try:
-				data_str = f.read()
-				data = orjson.loads(data_str)
 
-				affected = data.get("containers", {}).get("cna", {}).get("affected", [])
-				products = list([
-					a.get("product") for a in affected
-				])
-				vendors = list([
-					a.get("vendor") for a in affected
-				])
-				urls = re.findall(URL_REGEX, data_str)
-
-				for repo in repos:
-					repo.match_with(vendors, products, urls)
-
-			except UnicodeDecodeError as e:
-				print(e.reason)
-				print(f"ERROR loading {p}")
+def find_repo_matches(repos, cursor):
+	for repo in tqdm(repos, desc="Finding Repo Matches"):
+		test_vendor, test_product = repo.ids
+		url = repo.url
+		cursor.execute(FILTER_CVE_DATA, (f'%{test_vendor}%', f'%{url}%', f'{test_product}', f'{url}'))
+		rows = cursor.fetchall()
+		for row in rows:
+			repo.add_match(row)
 
 def generate_outputs(repos: list[Repo]):
 	output_missing = ""
@@ -129,19 +111,13 @@ def generate_outputs(repos: list[Repo]):
 
 	for repo in tqdm(repos, desc="Writing outputs"):
 		repo.resolve()
-		ven_len = len(repo.cve_vendor)
-		prod_len = len(repo.cve_product)
-		if ven_len + prod_len == 0:
-			output_missing += f"{repo.url}\n"
-		elif ven_len > 1 or prod_len > 1 or ven_len + prod_len == 1:
-			output_fix += f"{repo}\n"
-		else:
-			ven = repo.cve_vendor.pop() if ven_len > 0 else None
-			prod = repo.cve_product.pop() if prod_len > 0 else None
-			if ven and prod and ven != "n/a" and prod != "n/a":
-				output += f"{repo.url},{ven},{prod}\n"
-			else:
+		match repo.state:
+			case 0:
+				output_missing += f"{repo.url}\n"
+			case 1:
 				output_fix += f"{repo}\n"
+			case 2:
+				output += f"{repo.url},{repo.cve_vendor},{repo.cve_product}\n"
 
 	return output, output_missing, output_fix
 
@@ -156,10 +132,13 @@ def write_output(output, output_missing, output_fix):
 		f.write(output_fix)
 
 def main():
+	cursor = conn.cursor()
 	repos, id_map = read_data()
-	parse_jsons(repos)
+	find_repo_matches(repos, cursor)
 	output, output_missing, output_fix = generate_outputs(repos)
 	write_output(output, output_missing, output_fix)
+	cursor.close()
+	conn.close()
 
 if __name__ == "__main__":
 	main()
