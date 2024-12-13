f1ebbb6d081ad3e26555444849fcb2a8707d656d
Aiden Green <70248258+blaqat@users.noreply.github.com>
2024-12-13 07:23:22 -0500

    Added union aggregation for all pipelines

--- src/cve/mg_create_db_views.py
+++ src/cve/mg_create_db_views.py
@@ -1,14 +1,13 @@
 import os
-import re
-import orjson
-from tqdm import tqdm
 from config import mg_connect
+import re
 
 # Connection Details
 db = mg_connect()
 
 # Input files/folders
 cve_cwe_map = os.path.join(os.path.dirname(__file__), "pipelines/mongo-cve-cwe.py")
+cve_full_map = os.path.join(os.path.dirname(__file__), "pipelines/mongo-cve-full-map.py")
 patches_map = os.path.join(os.path.dirname(__file__), "pipelines/mongo-nvdcve.py")
 vendor_product_map = os.path.join(os.path.dirname(__file__), "pipelines/mongo-vendor-product.py")
 cve_vector_metrics_map = os.path.join(os.path.dirname(__file__), "pipelines/mongo-cve-metrics.py")
@@ -18,6 +17,10 @@ def load_pipeline(file):
     return eval(f.read())
 
 def create_view(name, pipeline):
+  try:
+    db.command("drop", name)
+  except:
+    pass
   db.command("create", name, pipeline=pipeline, viewOn="nvdcve")
 
 def main():
@@ -25,6 +28,8 @@ def main():
   create_view("cve_patches", load_pipeline(patches_map))
   create_view("cve_vendor_product", load_pipeline(vendor_product_map))
   create_view("cve_metrics", load_pipeline(cve_vector_metrics_map))
+  create_view("cve_full_map", load_pipeline(cve_full_map))
+  print("Views created successfully.")
 
 
 if __name__ == "__main__":
