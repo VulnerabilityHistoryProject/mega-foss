700118be32033a38f2de95e62e2256a327958b82
Trust-Worthy <jondeveloper0@gmail.com>
2025-02-10 10:23:14 -0500

    Created the new directory structure.

---
+++ src/slurm/pvcr/src/main.py
@@ -0,0 +1,50 @@
+import src.configuration.script_setup as script_setup
+import src.cve_utils.cve_config as cve_config
+import src.szz_utils.szz as szz
+import src.error_handling.handle_errors as handle
+
+
+import logging
+import json
+
+from typing import Any
+
+
+basic_logger = logging.getLogger("basic_logger")
+robust_logger = logging.getLogger("robust_logging")
+
+
+
+
+def process_JSON_CVE(json_file_path: str) -> cve_config.CVE:
+
+    processed_cve: set[str] = set() # o(1) lookup
+
+    with open(json_file_path, 'r') as file:
+        cve_data: list[dict[str, Any]] = json.load(file)
+
+    for cve_entry in cve_data:
+
+        json_cve_id:str = handle.safe_dict_get(cve_entry,"cve_id")
+        
+        repo: str = handle.safe_dict_get(cve_entry,"repo")
+        hash_patch_commit: str = handle.safe_dict_get(cve_entry,"commit")
+
+        if json_cve_id in processed_cve:
+            ## How can I associate the cve id with the existing CVE and corresponding cve object?
+            ## Create a class that has certain flags set for the type of vulnerability
+            ## maybe CVE inherits from a Vulnerability class!
+        
+            cve: cve_config.CVE = cve_config.CVE()
+
+            cve.cve_id = json_cve_id
+
+
+
+if __name__ == "__main__":
+
+    # Setup Basic logging cofiguration in case anything goes wrong during setup
+    script_setup.setup_initial_logging()
+
+    # Find the modified files by patch commit
+    szz.find_modified_files()
