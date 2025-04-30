1447f3fd40cd91fa4fa2dd137170e4d6b287bbfb
Trust-Worthy <jondeveloper0@gmail.com>
2025-04-29 20:48:47 -0400

    Finalized models that I will use for name and description. I am going with an ensemble approach.

--- src/cve/FOSS_composite_score/composite_score_scripts/embedding_pipeline/weaviate_config.py
+++ src/cve/FOSS_composite_score/composite_score_scripts/embedding_pipeline/weaviate_config.py
@@ -42,9 +42,19 @@ def define_weaviate_schema(client: weaviate.WeaviateClient) -> None:
         name="FOSSProject",
         description="Open source projects with name and description",
         vectorizer_config=[
-            wvc_config.Configure.NamedVectors.none(name="ollama_nomic_name_vectors"),
-            wvc_config.Configure.NamedVectors.none(name=""),
-            wvc_config.Configure.NamedVectors.none(name="combined_vector")
+            ### Named Vectors for FOSS project names / CVE vendor:product combos
+            wvc_config.Configure.NamedVectors.none(name="ollama_nomic_name_vec"),
+            wvc_config.Configure.NamedVectors.none(name="sbert_minilm_name_vec"),
+            wvc_config.Configure.NamedVectors.none(name="distil_bert_name_vec"),
+            wvc_config.Configure.NamedVectors.none(name="minilm_l6_v2_name_vec"),
+            wvc_config.Configure.NamedVectors.none(name="gte_large_name_vec"),
+
+            ### Named Vectors for FOSS project descriptions / CVE descriptions
+            wvc_config.Configure.NamedVectors.none(name="bge_large_description_vec"),
+            wvc_config.Configure.NamedVectors.none(name="e5_large_description_vec"),
+            wvc_config.Configure.NamedVectors.none(name="gte_large  _description_vec"),
+            wvc_config.Configure.NamedVectors.none(name="roberta_large_description_vec"),
+            wvc_config.Configure.NamedVectors.none(name="sbert_mpnet_base_v2_description_vec"),
         ],
         properties=[
             wvc_config.Property(name="name", data_type=wvc_config.DataType.TEXT, description="Name of the project"),
