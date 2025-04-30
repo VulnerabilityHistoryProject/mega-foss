6f81fc0eb71c1635a4142d5deeb8e34a05914caf
Trust-Worthy <jondeveloper0@gmail.com>
2025-04-29 20:03:14 -0400

    Working on defining the schema for my weaviate database. I want it to include multiple embedding models. I plan on using nomic to embed the FOSS project title and querying with the product:vendor combo from NVD. I plan on using 3-4 other models for the descriptions.

--- src/cve/FOSS_composite_score/composite_score_scripts/embedding_pipeline/weaviate_config.py
+++ src/cve/FOSS_composite_score/composite_score_scripts/embedding_pipeline/weaviate_config.py
@@ -8,6 +8,7 @@ incorporate different models.
 import os
 import weaviate
 from weaviate.classes.init import Auth
+import weaviate.classes.config as wvc_config
 from dotenv import load_dotenv
 
 def config_weaviate_db() -> bool:
@@ -23,15 +24,31 @@ def config_weaviate_db() -> bool:
     WEAVIATE_URL = os.getenv("WEAVIATE_URL")
     WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
 
-    # # Best practice: store your credentials in environment variables
-    # weaviate_url = os.environ["WEAVIATE_URL"]
-    # weaviate_api_key = os.environ["WEAVIATE_API_KEY"]
 
     # Connect to Weaviate Cloud
     client = weaviate.connect_to_weaviate_cloud(
-    cluster_url=WEAVIATE_URL,
-    auth_credentials=Auth.api_key(WEAVIATE_API_KEY),
+        cluster_url=WEAVIATE_URL,
+        auth_credentials=Auth.api_key(WEAVIATE_API_KEY),
     )
 
 
     return client.is_ready()
+
+def define_weaviate_schema(client: weaviate.WeaviateClient) -> None:
+    ""
+
+    # For Python client v4
+    foss_wvc_collection = client.collections.create(
+        name="FOSSProject",
+        description="Open source projects with name and description",
+        vectorizer_config=[
+            wvc_config.Configure.NamedVectors.none(name="ollama_nomic_name_vectors"),
+            wvc_config.Configure.NamedVectors.none(name=""),
+            wvc_config.Configure.NamedVectors.none(name="combined_vector")
+        ],
+        properties=[
+            wvc_config.Property(name="name", data_type=wvc_config.DataType.TEXT, description="Name of the project"),
+            wvc_config.Property(name="description", data_type=wvc_config.DataType.TEXT, description="Project description"),
+            wvc_config.Property(name="foss_hash", data_type=wvc_config.DataType.TEXT,description="Hash of FOSS project name")
+        ]
+    )
