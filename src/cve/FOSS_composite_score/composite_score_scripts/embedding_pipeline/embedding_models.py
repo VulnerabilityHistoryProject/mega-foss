b7a9e15973dd0cef9c7657fa8500377b0df6b9de
Trust-Worthy <jondeveloper0@gmail.com>
2025-04-30 14:57:10 -0400

    Making the models global varaibels in a separate interface file.

--- src/cve/FOSS_composite_score/composite_score_scripts/embedding_pipeline/embedding_models.py
+++ src/cve/FOSS_composite_score/composite_score_scripts/embedding_pipeline/embedding_models.py
@@ -1,6 +1,13 @@
 
 ### Models for embedding FOSS project names
 OLLAMA_NOMIC_EMBED_TEXT = 'nomic-embed-text'
+SBERT_MINILM = ""
+DISTIL_BERT = ""
+
 
 
 ### Models for embedding FOSS project descriptions
+
+
+### Both ###
+GTE_LARGE = ""
