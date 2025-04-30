de16b60f50163575b24abe7472334a4fcbdc6ca1
Trust-Worthy <jondeveloper0@gmail.com>
2025-04-30 11:47:55 -0400

    About to add the names of all the different models that I will use in embedding_models.py.

--- src/cve/FOSS_composite_score/composite_score_scripts/embedding_pipeline/embedding_models.py
+++ src/cve/FOSS_composite_score/composite_score_scripts/embedding_pipeline/embedding_models.py
@@ -1,1 +1,6 @@
-NOMIC_EMBED_TXT = 'nomic-embed-text'
+
+### Models for embedding FOSS project names
+OLLAMA_NOMIC_EMBED_TEXT = 'nomic-embed-text'
+
+
+### Models for embedding FOSS project descriptions
--- src/cve/FOSS_composite_score/composite_score_scripts/embedding_pipeline/nomic_embed.py
+++ src/cve/FOSS_composite_score/composite_score_scripts/embedding_pipeline/nomic_embed.py
@@ -1,19 +1,30 @@
 """
-Using nomic-embed-text-v1.5.
+Using nomic-embed-text-v1.5 to embed short 2-3 word prompts.
+Vectorized prompts will be stored in a weaviate vector database for later analysis.
 
-Returns:
-    _type_: _description_
+Author: @Trust-Worthy
+
+
 """
 
 import ollama
 
-from embedding_models import NOMIC_EMBED_TXT
+from embedding_models import OLLAMA_NOMIC_EMBED_TEXT
 
 
 def embed_prompt_with_nomic(prompt: str) -> list[float]:
+    """
+    Embed a prompt using nomic-embed-text-v1.5
+
+    Args:
+        prompt (str): Desired text to be embedded using the nomic-embed-text model.
+
+    Returns:
+        list[float]: Vectorized representation of the prompt. Either 768 vectors or 8192 depending on the model
+    """
 
     response = ollama.embeddings(
-        model=NOMIC_EMBED_TXT,
+        model=OLLAMA_NOMIC_EMBED_TEXT,
         prompt=prompt
     )
 
