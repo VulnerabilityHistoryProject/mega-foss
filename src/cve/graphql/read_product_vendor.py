227fc8f9d47a2b369583c3e67f42c32bde8b5259
PopPaolo <pp6876@rit.edu>
2025-06-11 18:51:27 -0400

    Only include each vendor/product once

--- src/cve/graphql/read_product_vendor.py
+++ src/cve/graphql/read_product_vendor.py
@@ -6,5 +6,12 @@ with open('lists/nvdcve-vendor-product.json') as f:
 #with open('src/cve/graphql/trial.json') as f:
     #data = json.load(f)
     
-extracted_pairs = [{'vendor': item.get('vendor'), 'product': item.get('product')} for item in data if item.get('vendor') is not None and item.get('product') is not None]
-print(extracted_pairs)
+seen = set()
+extracted_pairs = []
+
+# Extract unique vendor-product pairs from the JSON data
+for pair in [{'vendor': item.get('vendor'), 'product': item.get('product')} for item in data if item.get('vendor') is not None and item.get('product') is not None]:
+    key = (pair['vendor'], pair['product'])
+    if key not in seen:
+        seen.add(key)
+        extracted_pairs.append(pair)
