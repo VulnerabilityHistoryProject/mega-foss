bb32df2619e4e267ac56f085b20b1383a95dc7d6
Andy Meneely <andy@se.rit.edu>
2025-08-04 10:30:26 -0400

    some tweaks to make

--- src/slurm/drill_scripts/test_scripts/patch_dataset_script.py
+++ src/slurm/drill_scripts/test_scripts/patch_dataset_script.py
@@ -13,6 +13,10 @@ import subprocess
 import json
 
 
+# TODO add a constant string that points to the folder of cloned repos
+# Assume that the clone repos have the folder structure of:
+# nameoforg/nameofrepo or username/nameofrepo
+
 from cve.graphql.make_gql_queries import get_repo_url
 
 with open('src/slurm/drill_scripts/test_scripts/test_patch_vuln_match.json', 'r') as file:
@@ -25,12 +29,12 @@ placeholder_repo_folder = "placeholder_cloned_repos"
    The script needs to:
         Get the repo url
         Get the cve id
-        Clone the repo in the temporary folder 
+        Clone the repo in the temporary folder
         Used pydriller on the cloned repo and output the results in a folder named after the cve_id
         Same with the new
         Use pydriller to get the diff file
-        
-    Repeat 
+
+    Repeat
 """
 
 def get_cve_id(vendor,product):
@@ -50,12 +54,13 @@ def get_vuln_hashes(vendor, product):
 def make_cve_folder(cve_id):
     #path to pydriller-cve inside mega-foss
     pydriller_cve_dir = os.path.join(root_path, 'pydriller-cve')
-    
+
     #path to the CVE-specific folder
     cve_folder_path = os.path.join(pydriller_cve_dir, cve_id)
     os.makedirs(cve_folder_path, exist_ok=True)
     return cve_folder_path
 
+# TODO - no need to clone and delete, just assume we have it
 
 def clone_the_repo(repo_url):
     repo_name = repo_url.split("/")[-1].replace(".git", "")
@@ -64,7 +69,7 @@ def clone_the_repo(repo_url):
         #subprocess.run(["git", "checkout", commit_hash], cwd=placeholder_repo_folder, check=True)
     except subprocess.CalledProcessError as e:
         print(f"failed cloning {repo_name}: {e}")
-    
+
 def delete_cloned_repo(repo_path):
     if os.path.exists(repo_path) and os.path.isdir(repo_path):
         shutil.rmtree(repo_path)
@@ -80,9 +85,19 @@ def script(vendor,product):
     get_commit_by_hash(placeholder_repo_folder,"771b3d8749b34b6eea4e03a2e514380da9582f90",vuln_hashes,folder_path)
     delete_cloned_repo(placeholder_repo_folder)
 
+# TODO Structure it this way:
+#  CVE-2008-1897/
+#     771b3d8749b34b6eea4e03a2e514380da9582f90/
+#         vulnerable_chan_iax2.c
+#         fixed_chan_iax2.c
+#         CVE-2008-1897_771b3d8749b34b6eea4e03a2e514380da9582f90.patch
+#     a8b180875b037b8da26f6a3bcc8e5e98b8c904d2/
+#   ...
+
+
 def get_commit_by_hash(repo_path, patch_hash, vuln_commit_dict, output_dir):
     patch_commit = next(Repository(repo_path, single=patch_hash).traverse_commits())
-    
+
     for mod in patch_commit.modified_files:
         print(f"Patch File: {mod.filename}")
         patch_code = mod.source_code or ""
@@ -97,7 +112,7 @@ def get_commit_by_hash(repo_path, patch_hash, vuln_commit_dict, output_dir):
             try:
                 commit = next(Repository(repo_path, single=commit_hash).traverse_commits())
                 for mod in commit.modified_files:
-                    if mod.filename.endswith(filename.split("/")[-1]):  
+                    if mod.filename.endswith(filename.split("/")[-1]):
                         print(f"Vuln File: {mod.filename}")
                         vuln_code = mod.source_code or ""
                         vuln_path = os.path.join(output_dir, f"vuln_{i}_{mod.filename}")
@@ -114,11 +129,10 @@ def get_commit_by_hash(repo_path, patch_hash, vuln_commit_dict, output_dir):
         os.makedirs(os.path.dirname(diff_path), exist_ok=True)
         with open(diff_path, "w", encoding="utf-8") as f:
             f.write(diff_code)
-    
-        
+
+
 
 if __name__ == "__main__":
 
     script("jcollie","asterisk")
-    
-    
+
